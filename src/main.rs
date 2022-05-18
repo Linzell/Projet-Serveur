0d03b336c6e3f7de1d9759b402479230b2f42c79
Charlie Cohen <linzellart@gmail.com>
2022-05-18 14:45:33 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::fs;
 
 fn main() {
     let ecouteur = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +9,22 @@ fn main() {
     for flux in ecouteur.incoming() {
         let flux = flux.unwrap();
 
-        println!("Connexion établie !");
+        gestion_connexion(flux);
     }
+}
+
+fn gestion_connexion(mut flux: TcpStream) {
+  let mut tampon = [0; 1024];
+  flux.read(&mut tampon).unwrap();
+
+  let contenu = fs::read_to_string("../hello.html").unwrap();
+
+  let reponse = format!(
+      "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+      contenu.len(),
+      contenu
+  );
+
+  flux.write(reponse.as_bytes()).unwrap();
+  flux.flush().unwrap();
 }
